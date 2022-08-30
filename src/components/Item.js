be1c1d567ea9242bedfa9a69256b1b6896588da0
OteroBarbara <oterobarbara251@gmail.com>
2022-08-29 23:00:51 -0300

    Entregable - Arreglo para que el color en la parte del stock coincida con la demo

--- src/components/Item.js
+++ src/components/Item.js
@@ -20,7 +20,7 @@ export default function Item(props) {
       <p>
       {props.producto.descripcion}
       </p>
-      <h5>En stock: <span>{stock>0?stock:'Agotado'}</span></h5>
+      <h5>En stock: {stock>0?stock:<span>Agotado</span>}</h5>
       <button onClick={() =>{setStock(stock-1);props.fnCarrito();}} disabled={stock===0}>{stock>0?'COMPRAR':'SIN STOCK'}</button>
     </div>
   )
